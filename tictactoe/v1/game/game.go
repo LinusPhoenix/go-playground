a046d22480567884ecbd84d35ba5ecb56b25b142
Linus Eckhard <leckhard@architrave.de>
2022-11-17 13:51:56 +0100

    Implement tictactoe  v1

---
+++ tictactoe/v1/game/game.go
@@ -0,0 +1,211 @@
+package game
+
+import (
+	"errors"
+	"fmt"
+)
+
+type CellState int
+
+func (c CellState) String() string {
+	switch c {
+	case None:
+		return " "
+	case X:
+		return "X"
+	case O:
+		return "O"
+	}
+	return "Unknown CellState!"
+}
+
+const (
+	None CellState = iota
+	X    CellState = iota
+	O    CellState = iota
+)
+
+type move struct {
+	x      int
+	y      int
+	player CellState
+}
+
+type Game struct {
+	// The tictactoe board.
+	// First dimension represents rows, second dimension represents columns.
+	// [0][0] is the cell in the top left-hand corner,
+	// [2][2] is the cell in the bottom right-hand corner.
+	board [3][3]CellState
+	// While the game is active, swaps between X and O. X goes first.
+	// Once the game is over, this is set to None
+	playerActive CellState
+	moveLast     *move
+	// While the game is going on, this is false, and true when the game is finished.
+	gameOver bool
+	// While gameOver is false, this is set to None.
+	// When gameOver is true, this is X or O if there is a winner, or None is the game is a draw.
+	winner CellState
+}
+
+func New() *Game {
+	var board [3][3]CellState
+	for i, row := range board {
+		for j := range row {
+			board[i][j] = None
+		}
+	}
+
+	return &Game{board, X, nil, false, None}
+}
+
+func (game *Game) GetPlayerActive() CellState {
+	return game.playerActive
+}
+
+func (game *Game) GetCellState(x int, y int) (CellState, error) {
+	if !areCoordsInBounds(x, y) {
+		return None, errors.New("Invalid move. Each coordinate must be in the interval [0, 2]")
+	}
+	return game.board[x][y], nil
+}
+
+func (game *Game) IsGameOver() bool {
+	return game.gameOver
+}
+
+func (game *Game) GetWinner() CellState {
+	return game.winner
+}
+
+func (game *Game) MakeTurn(x int, y int) error {
+	if game.gameOver {
+		return errors.New("The game is over. No more moves can be made")
+	}
+	if !areCoordsInBounds(x, y) {
+		return errors.New("Invalid move. Each coordinate must be in the interval [0, 2]")
+	}
+
+	if cell := game.board[x][y]; cell != None {
+		return fmt.Errorf("Invalid move. The cell at [%d][%d] is already claimed by %#v", x, y, cell)
+	}
+	game.board[x][y] = game.playerActive
+	game.moveLast = &move{x, y, game.playerActive}
+
+	switch game.playerActive {
+	case X:
+		game.playerActive = O
+	case O:
+		game.playerActive = X
+	}
+
+	isOver, winner := game.isGameOver()
+	game.gameOver = isOver
+	game.winner = winner
+
+	return nil
+}
+
+func (game *Game) isGameOver() (bool, CellState) {
+	// If there is no last move, there can be no winner.
+	if game.moveLast == nil {
+		return false, None
+	}
+
+	// Only the player who made the last move can win.
+	winCandidate := game.moveLast.player
+	x := game.moveLast.x
+	y := game.moveLast.y
+
+	// Should the last move be made by None, there can be no winner.
+	if winCandidate == None {
+		return false, None
+	}
+
+	// We need to check the row, column, and diagonals for a potential win.
+	row := game.checkRowForWinner(x, y, winCandidate)
+	column := game.checkColumnForWinner(x, y, winCandidate)
+	diagonal := game.checkDiagonalForWinner(x, y, winCandidate)
+	antidiagonal := game.checkAntidiagonalForWinner(x, y, winCandidate)
+	if row == winCandidate || column == winCandidate || diagonal == winCandidate || antidiagonal == winCandidate {
+		return true, winCandidate
+	}
+
+	isDraw := true
+	for i, row := range game.board {
+		for j := range row {
+			// If any cell is still None, then the game is not a draw.
+			isDraw = isDraw && game.board[i][j] != None
+		}
+	}
+	return isDraw, None
+}
+
+func (game *Game) checkRowForWinner(x int, y int, winCandidate CellState) CellState {
+	if winCandidate == None {
+		return None
+	}
+	for j := 0; j <= 2; j++ {
+		if game.board[x][j] != winCandidate {
+			return None
+		}
+	}
+
+	return winCandidate
+}
+
+func (game *Game) checkColumnForWinner(x int, y int, winCandidate CellState) CellState {
+	if winCandidate == None {
+		return None
+	}
+
+	for i := 0; i <= 2; i++ {
+		if game.board[i][y] != winCandidate {
+			return None
+		}
+	}
+
+	return winCandidate
+}
+
+func (game *Game) checkDiagonalForWinner(x int, y int, winCandidate CellState) CellState {
+	if winCandidate == None {
+		return None
+	}
+	// If x does not equal y, the move was not made on a diagonal.
+	if x != y {
+		return None
+	}
+
+	// Check top left to bottom right
+	for i := 0; i <= 2; i++ {
+		if game.board[i][i] != winCandidate {
+			return None
+		}
+	}
+
+	return winCandidate
+}
+
+func (game *Game) checkAntidiagonalForWinner(x int, y int, winCandidate CellState) CellState {
+	if winCandidate == None {
+		return None
+	}
+	// If x does not equal y, the move was not made on a diagonal.
+	if x != y {
+		return None
+	}
+
+	// Check bottom left to top right
+	for i := 0; i <= 2; i++ {
+		if game.board[2-i][i] != winCandidate {
+			return None
+		}
+	}
+
+	return winCandidate
+}
+
+func areCoordsInBounds(x int, y int) bool {
+	return x >= 0 || x <= 2 && y >= 0 || y <= 2
+}
---
+++ tictactoe/v1/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"linusphoenix/tictactoe/v1/game"
+	"os"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// Goals of this version:
+// - Use bubbletea for a TUI, no premade UI components
+// - User plays both players
+
+type model struct {
+	game    *game.Game
+	cursorX int
+	cursorY int
+	err     error
+}
+
+func initialModel() model {
+	game := game.New()
+	cursorX, cursorY := 0, 0
+	return model{game, cursorX, cursorY, nil}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up":
+			if m.cursorX > 0 {
+				m.cursorX--
+			}
+		case "down":
+			if m.cursorX < 2 {
+				m.cursorX++
+			}
+
+		case "left":
+			if m.cursorY > 0 {
+				m.cursorY--
+			}
+		case "right":
+			if m.cursorY < 2 {
+				m.cursorY++
+			}
+
+		// The "enter" key makes the player take their turn.
+		case "enter":
+			if !m.game.IsGameOver() {
+				m.err = m.game.MakeTurn(m.cursorX, m.cursorY)
+				if m.game.IsGameOver() {
+					return m, tea.Quit
+				}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Use the arrow keys to navigate on the board.\n"
+	s += "Use enter to make your move.\n\n"
+
+	playerActive := m.game.GetPlayerActive()
+	gameOver := m.game.IsGameOver()
+	s += fmt.Sprintf("Active player: %v\n\n", playerActive)
+
+	// Render the board row by row.
+	for i := 0; i <= 2; i++ {
+		// Render each cell.
+		for j := 0; j <= 2; j++ {
+			if j != 0 {
+				s += " "
+			}
+			cellState, _ := m.game.GetCellState(i, j)
+			s += fmt.Sprintf("[%v]", cellState)
+		}
+		s += "\n"
+		// If this is the cursor's row, render the cursor. Otherwise, render an empty row.
+		if !gameOver && i == m.cursorX {
+			// The cursor is either the 2nd, 5th, or 10th position in the row.
+			// So we can multiply cursorY by 4 and append that many plus one to spaces to the string.
+			s += strings.Repeat(" ", m.cursorY*4+1) + "^\n"
+		} else {
+			s += "\n"
+		}
+	}
+
+	// Render the current error, if any, from the game.
+	if m.err != nil {
+		s += fmt.Sprintf("%v.\n", m.err)
+	} else {
+		s += "\n"
+	}
+
+	// Announce the winner. Otherwise, explain how to quit.
+	if gameOver {
+		winner := m.game.GetWinner()
+		if winner == game.None {
+			s += "The game is a draw!\n"
+		} else {
+			s += fmt.Sprintf("%v wins!\n", winner)
+		}
+	} else {
+		s += "\nPress q to quit.\n"
+	}
+
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
